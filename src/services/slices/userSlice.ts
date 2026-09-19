34a8d292ea125b004fc0b69c86072d38abdb87b8
Mark Laurovsky <shaihilaev_murad@mail.ru>
2026-09-19 21:34:12 +0300

    добавил в слайс thunk updateUser дописал extraReducers в слайс под апдейт юзера, настройл селекторы

--- src/services/slices/userSlice.ts
+++ src/services/slices/userSlice.ts
@@ -1,4 +1,4 @@
-import { getUserApi } from '@api';
+import { getUserApi, TRegisterData, updateUserApi } from '@api';
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { TUser } from '@utils-types';
 import type { RootState } from '../store';
@@ -6,17 +6,26 @@ import type { RootState } from '../store';
 type UserState = {
   user: TUser | null;
   isLoading: boolean;
-  error: string | null;
+  isUpdating: boolean;
+  loadError: string | null;
+  updateError: string | null;
 };
 
 const initialState: UserState = {
   user: null,
   isLoading: false,
-  error: null
+  isUpdating: false,
+  loadError: null,
+  updateError: null
 };
 
 export const getUserThunk = createAsyncThunk('user/getUser', () =>
   getUserApi()
+);
+
+export const updateUserThunk = createAsyncThunk(
+  'user/updateUser',
+  (user: Partial<TRegisterData>) => updateUserApi(user)
 );
 
 const userSlice = createSlice({
@@ -26,16 +35,29 @@ const userSlice = createSlice({
   extraReducers(builder) {
     builder.addCase(getUserThunk.pending, (state) => {
       state.isLoading = true;
-      state.error = null;
+      state.loadError = null;
     });
     builder.addCase(getUserThunk.fulfilled, (state, action) => {
       state.user = action.payload.user;
       state.isLoading = false;
-      state.error = null;
+      state.loadError = null;
     });
     builder.addCase(getUserThunk.rejected, (state, action) => {
       state.isLoading = false;
-      state.error = action.error.message || 'Произошла ошибка';
+      state.loadError = action.error.message || 'Произошла ошибка';
+    });
+    builder.addCase(updateUserThunk.pending, (state) => {
+      state.isUpdating = true;
+      state.updateError = null;
+    });
+    builder.addCase(updateUserThunk.fulfilled, (state, action) => {
+      state.user = action.payload.user;
+      state.isUpdating = false;
+      state.updateError = null;
+    });
+    builder.addCase(updateUserThunk.rejected, (state, action) => {
+      state.isUpdating = false;
+      state.updateError = action.error.message || 'Не удалось обновить профиль';
     });
   }
 });
@@ -44,6 +66,10 @@ export const selectUser = (state: RootState) => state.user.user;
 
 export const selectUserIsLoading = (state: RootState) => state.user.isLoading;
 
-export const selectUserError = (state: RootState) => state.user.error;
+export const selectUserIsUpdating = (state: RootState) => state.user.isUpdating;
+
+export const selectUserError = (state: RootState) => state.user.loadError;
+
+export const selectUpdateUserError = (state: RootState) => state.user.updateError;
 
 export default userSlice.reducer;
