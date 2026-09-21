8b03889502df3200ca0ff75946d160fb2134e0d2
Mark Laurovsky <shaihilaev_murad@mail.ru>
2026-09-21 20:01:50 +0300

    дописал protectedRoute

--- src/components/protected-route/protected-route.tsx
+++ src/components/protected-route/protected-route.tsx
@@ -1,9 +1,34 @@
-import { Children } from "react";
+import {
+  selectUser,
+  selectUserIsLoading
+} from '../../services/slices/userSlice';
+import { useSelector } from '../../services/store';
+import { Preloader } from '@ui';
+import { Navigate } from 'react-router-dom';
 
 type ProtectedRouteProps = {
-    children: React.ReactElement;
-}
+  children: React.ReactElement;
+  onlyUnAuth?: boolean;
+};
 
-export const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
-    return children;
-}
+export const ProtectedRoute = ({
+  children,
+  onlyUnAuth = false
+}: ProtectedRouteProps) => {
+  const user = useSelector(selectUser);
+  const isLoading = useSelector(selectUserIsLoading);
+
+  if (isLoading) {
+    return <Preloader />;
+  }
+
+  if (!onlyUnAuth && !user) {
+    return  <Navigate to='/login' replace />;
+  }
+
+  if (onlyUnAuth && user) {
+    return  <Navigate to='/' replace />;
+  }
+
+  return children;
+};
--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -1,18 +1,42 @@
-import { FC, SyntheticEvent, useState } from 'react';
+import { FC, SyntheticEvent, useEffect, useState } from 'react';
 import { RegisterUI } from '@ui-pages';
+import { useDispatch, useSelector } from '../../services/store';
+import {
+  registerUserThunk,
+  selectRegisterError,
+  selectUser
+} from '../../services/slices/userSlice';
+import { useNavigate } from 'react-router-dom';
 
 export const Register: FC = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector(selectUser);
+  const error = useSelector(selectRegisterError);
   const [userName, setUserName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  useEffect(() => {
+    if (user) {
+      navigate('/');
+    }
+  }, [user, navigate]);
+
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
+    dispatch(
+      registerUserThunk({
+        name: userName,
+        email,
+        password
+      })
+    );
   };
 
   return (
     <RegisterUI
-      errorText=''
+      errorText={error || ''}
       email={email}
       userName={userName}
       password={password}
